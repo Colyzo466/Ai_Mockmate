7b208da64e451ef32832a3eedfe5976bec5f2044
victor oguike <colyzo466@gmail.com>
2025-05-03 07:54:17 +0100

    implement ai Agent

--- lib/vapi.sdk.ts
+++ lib/vapi.sdk.ts
@@ -1,3 +1,8 @@
 import Vapi from "@vapi-ai/web";
 
-export const vapi = new Vapi(process.env.NEXT_PUBLIC_VAPI_WEB_TOKEN!);
+if (!process.env.NEXT_PUBLIC_VAPI_WEB_TOKEN) {
+    throw new Error("Missing NEXT_PUBLIC_VAPI_WEB_TOKEN. Check your .env.local file.");
+  }
+  
+  export const vapi = new Vapi(process.env.NEXT_PUBLIC_VAPI_WEB_TOKEN!);
+  
